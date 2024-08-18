3f3d7cbbb51b5cf9c3c38852e0dba4792b89e5c6
Ihab Bourgi <116211409+Abhi94e@users.noreply.github.com>
2024-08-18 21:30:04 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 document.querySelector('.scroll-down a').addEventListener('click', function (e) {
     e.preventDefault();
     document.querySelector('#target-section').scrollIntoView({ behavior: 'smooth' });
